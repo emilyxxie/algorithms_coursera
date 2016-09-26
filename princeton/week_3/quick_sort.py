1c8a9ca7dae1dee9f8db76f10ff1d086983a5b70
Emily Xie <emilyxxie@gmail.com>
2016-09-26 17:40:11 -0400

    quick sort fixed

--- princeton/week_3/quick_sort.py
+++ princeton/week_3/quick_sort.py
@@ -2,7 +2,7 @@ import random
 
 # quick sort - goes through and sorts the pivots in place
 def quick_sort(n):
-  low = 1
+  low = 0
   high = len(n) - 1
   random.shuffle(n)
   sort(n, low, high)
@@ -13,14 +13,14 @@ def sort(n, low, high):
     return
   sorted_pivot = partition(n, low, high)
   sort(n, low, sorted_pivot - 1)
-  sort(n, high, sorted_pivot + 1)
+  sort(n, sorted_pivot + 1, high)
 
 # pick partition point. traverse the array using two pointers, a + b
 # and swap array[a] and array[b] if they are on the wrong side of partition
 # until the two indexes cross
 def partition(n, low, high):
-  partition = n[0]
-  a = low
+  partition = n[low]
+  a = low + 1
   b = high
   while a < b:
     while n[a] < partition:
@@ -34,7 +34,8 @@ def partition(n, low, high):
     if a > b:
       break
     n[a], n[b] = n[b], n[a]
-  n[b], n[0] = n[0], n[b]
+  if n[b] < n[low]:
+    n[b], n[low] = n[low], n[b]
   return b
 
 n = random.sample(range(0, 100), 100)
