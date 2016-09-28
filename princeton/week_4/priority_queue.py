c8ba4fb6d386a4d3b055d2e64e8f80e59d16f211
Emily Xie <emilyxxie@gmail.com>
2016-09-28 18:16:23 -0400

    Priority queue refactored and looking better

--- princeton/week_4/priority_queue.py
+++ princeton/week_4/priority_queue.py
@@ -9,13 +9,13 @@ A heap is a binary tree with a few special properties:
      - Binary heap is a complete binary tree.
      - This means all nodes of a level must be filled out before moving on to the next
   2) Heap property
-     - Conforms to an ordering: all parent nodes will have the same size property in relation to all of its children
-     - So, min heap -> all parents will be smaller than children, max heap -> all parents larger than children
+     - Conforms to an ordering: all parent nodes will have the same size property in relation to all of its cren
+     - So, min heap -> all parents will be smaller than cren, max heap -> all parents larger than cren
 
-Array structure: represent nodes of children using arithmetic -> all children's nodes are 2k and 2k + 1
+Array structure: represent nodes of cren using arithmetic -> all cren's nodes are 2k and 2k + 1
 
-Children's order with respect to each other does not matter--> all works out in the end since popping out of queue takes the top node.
-Top node is always floated based on two children, which are continually floated to the top.
+cren's order with respect to each other does not matter--> all works out in the end since popping out of queue takes the top node.
+Top node is always floated based on two cren, which are continually floated to the top.
 
 Traversing through the priority queue via insertion or removal should take O(log N).
 
@@ -46,26 +46,25 @@ class MaxPriorityQueue(object):
 
   def sink(self):
     i = 1
-    c1 = i * 2
-    while c1 < len(self.heap):
-      larger_child = c1 if self.heap[c1] > self.heap[c2] else c2
-      if self.heap[i] < self.heap[larger_child]:
-        self.heap[i], self.heap[larger_child] = self.heap[larger_child], self.heap[i]
-        if larger_child % 2 == 0:
-          i = c1
-        else:
-          i = c2
-        c1 *= 2
-        c2 *= 2
-      else:
-        return
+    c = i * 2
+    size = len(self.heap)
+    while c < size:
+      larger_c = c
+      # checks to see if second child exists and is larger
+      if c + 1 < size and self.heap[c + 1] > self.heap[c]: larger_c += 1
+      self.heap[i], self.heap[larger_c] = self.heap[larger_c], self.heap[i]
+      i = c
+      c *= 2
 
 
+# test
 queue = MaxPriorityQueue()
+
 queue.insert(15)
 queue.insert(12)
 queue.insert(10)
 queue.insert(5)
 queue.insert(3)
 queue.remove_max()
+
 print(queue.heap)
