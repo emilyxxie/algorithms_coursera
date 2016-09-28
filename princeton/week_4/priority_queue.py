d55eab70c3fb593d52a6590700a264c625e5db8b
Emily Xie <emilyxxie@gmail.com>
2016-09-28 18:25:06 -0400

    priority queue implemented - edit notes

--- princeton/week_4/priority_queue.py
+++ princeton/week_4/priority_queue.py
@@ -9,13 +9,13 @@ A heap is a binary tree with a few special properties:
      - Binary heap is a complete binary tree.
      - This means all nodes of a level must be filled out before moving on to the next
   2) Heap property
-     - Conforms to an ordering: all parent nodes will have the same size property in relation to all of its cren
-     - So, min heap -> all parents will be smaller than cren, max heap -> all parents larger than cren
+     - Conforms to an ordering: all parent nodes will have the same size property in relation to all of its children
+     - So, min heap -> all parents will be smaller than children, max heap -> all parents larger than children
 
-Array structure: represent nodes of cren using arithmetic -> all cren's nodes are 2k and 2k + 1
+Array structure: represent nodes of children using arithmetic -> all children's nodes are 2k and 2k + 1
 
-cren's order with respect to each other does not matter--> all works out in the end since popping out of queue takes the top node.
-Top node is always floated based on two cren, which are continually floated to the top.
+children's order with respect to each other does not matter--> all works out in the end since popping out of queue takes the top node.
+Top node is always floated based on two children, which are continually floated to the top.
 
 Traversing through the priority queue via insertion or removal should take O(log N).
 
