07577c5900ca0a516250371a91cef9abeb4a35b5
Emily Xie <emilyxxie@gmail.com>
2016-09-29 14:53:12 -0400

    heap sort error fixed

--- princeton/week_4/heap_sort.py
+++ princeton/week_4/heap_sort.py
@@ -1,13 +1,14 @@
 '''
 
 Implement the heap-sort algorithm for efficient sorting.
-Two phases:
+
+Comprises of two main phases:
   1) Construction phase
     First, implement bottom-up heap order
   2) Swap max item with smallest, then, continually sink until array has been sorted
 
-Heapsort time complexity is O(N log N). Construction takes 2N compares and exchanges up front.
-It's an nLogn in-place sorting algorithm.
+Heapsort time complexity is O(N log N): construction takes 2N up front + whereas swapping takes log N time
+Primary advantage is that it's a guaranteed nLogn in-place sorting algorithm.
 
 '''
 
@@ -33,7 +34,6 @@ class HeapSort(object):
   def order_heap(self):
     size = len(self.heap)
     for i in range(size // 2, 0, -1):
-      print("Index: %d" % (i))
       self.__sink(i, size)
 
   def sort(self):
@@ -41,13 +41,14 @@ class HeapSort(object):
     ordered = 1
     while ordered < len(self.heap):
       self.heap[1], self.heap[-ordered] = self.heap[-ordered], self.heap[1]
-      self.__sink(1, ordered)
+      self.__sink(1, len(self.heap) - ordered)
       ordered += 1
 
-# reliable broken case -> order heap does not work
-array = [1, 7, 5, 11, 12, 93, 21]
+array = random.sample(
+  range(0, 100),
+  100
+)
 
-# array = random.sample(range(0, 10), 10)
 heap_sort = HeapSort(array)
 heap_sort.sort()
 print(heap_sort.heap)
