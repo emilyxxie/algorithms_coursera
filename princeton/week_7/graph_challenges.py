fd37538b89bced063556dcbc535f090b6b0ac022
Emily Xie <emilyxxie@gmail.com>
2016-10-23 21:58:39 -0400

    Euler tour complete

--- princeton/week_7/graph_challenges.py
+++ princeton/week_7/graph_challenges.py
@@ -8,27 +8,6 @@ A couple of simple graph challenges below, as proposed by Professor Sedgewick.
 from graph import Graph, Vertex
 import sys
 
-graph = Graph()
-
-# for i in range(1, 17):
-#   graph.add_vertex(i, str(i) * 3)
-
-# graph.add_edge(1, 2)
-# graph.add_edge(1, 4)
-# graph.add_edge(1, 3)
-# graph.add_edge(2, 7)
-# graph.add_edge(4, 5)
-# graph.add_edge(3, 10)
-# graph.add_edge(7, 8)
-# graph.add_edge(7, 6)
-# graph.add_edge(6, 9)
-# graph.add_edge(4, 7)
-# graph.add_edge(11, 13)
-# graph.add_edge(11, 14)
-# graph.add_edge(15, 16)
-# graph.add_edge(1, 2)
-# graph.display()
-
 
 '''
 
@@ -36,6 +15,7 @@ Determine whether or not a graph is a bipartite.
 
 '''
 
+# TODO
 
 
 
@@ -62,9 +42,6 @@ def does_graph_cycle(graph):
     dfs(v_key, None)
   return "No cycle"
 
-# does_graph_cycle(graph)
-
-
 '''
 
 Seven bridges of Konisberg:
@@ -83,35 +60,6 @@ available to leave that vertex
 Do not use an edge to go to a vertex unless anpther edge is available.
 
 '''
-
-for i in range(1, 7):
-  graph.add_vertex(i, str(i) * 3)
-
-# has path, complicated:
-# graph.add_edge(1, 2)
-# graph.add_edge(2, 3)
-# graph.add_edge(2, 4)
-# graph.add_edge(3, 5)
-# graph.add_edge(4, 5)
-# graph.add_edge(5, 2)
-# graph.add_edge(5, 6)
-
-# # has path
-# graph.add_edge(1, 2)
-# graph.add_edge(1, 4)
-# graph.add_edge(1, 3)
-# graph.add_edge(3, 2)
-# graph.add_edge(3, 4)
-
-# has path with no odds
-graph.add_edge(1,2)
-graph.add_edge(2,3)
-graph.add_edge(3,4)
-graph.add_edge(4,1)
-
-
-graph.display()
-
 
 
 def find_euler_tour(graph):
@@ -142,32 +90,18 @@ def find_euler_tour(graph):
         dfs(c, key)
 
   dfs(start, None)
-  if len(edges_visited) != len(graph.vertices):
-    return False
+
+  # Final validation check:
+  # If after the DFS, items remained untraveled, this means there are unconnected vertices
+  edges_visited = dict((key, val) for key, val in edges_visited.iteritems() if val)
+  if len(edges_visited) != len(graph.vertices): return False
   return tour
 
 
-'''
-
-  # don't do this check if we're on the final item
-  # edges may not always have been visited
-
-  # after the DFS, if there are any unvisited nodes left,
-  # this means that there are unvisited vertices
-  # this means not all nodes are connectedd
-  # this means that this is not a euler tour
-  # I want to roll this check into validate_euler_tour() function
-
-'''
-
-
-# validates if a graph is a euler tour or not
-# checks the criteria that there are only either 0 or 2 vertices
-# with odd connections
-# if so, return the start and end nodes
-# in the case where there are no odds
-# the start and end nodes will be any same node
-# to indicate that the graph will start and end at the same place
+# Validates if a graph is a euler tour or not
+# by ensuring there are only 0 or 2 odd vertices.
+# If valid, return the start and end vertices.
+# If no odds, the start and end vertices will be the same vertex
 def validate_euler_tour(graph):
   odd = []
   for key in graph.vertices:
@@ -201,27 +135,6 @@ Other algorithms to note:
     This is a really complex algorithm, and would never be expected of a student.
 
 '''
-
-
-
-  # stores node, and the connections that it already has visited
-  # first find if it is a euler problem
-  # find the starting and end vertex
-  # cycle through the graph, get each vertex
-  # check the connections.
-  # use DFS -> only go to the next connection if you look it up
-  # in the hash, and see that it has avialable connections to leave
-  # unless it is the final one
-
-  # for key in graph.vertices(graph):
-    # while cycling through,
-    # if there's
-    # if there's an additional odd,
-    # we need to make sure that the odd is the final one
-    # if there's an odd node that we find
-    # we wipe edges visited
-    # and start the cycle anew with the odd vertex
-    #
 
 
 
